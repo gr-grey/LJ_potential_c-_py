260f9b282f91122e152267f0246cdbfd07b9fa6c
Grey <grui@sas.upenn.edu>
2017-08-01 14:07:42 -0400

    add README info, build files and compile

---
+++ build/CMakeFiles/LJ_potential.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Grey/Desktop/LJ_potential_c-_py/LJ_potential.cpp" "/Users/Grey/Desktop/LJ_potential_c-_py/build/CMakeFiles/LJ_potential.dir/LJ_potential.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/Grey/miniconda/envs/sss/include"
+  "/Users/Grey/miniconda/envs/sss/include/python3.5m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/LJ_potential.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LJ_potential.dir/LJ_potential.cpp.o"
+  "LJ_potential.pdb"
+  "LJ_potential.cpython-35m-darwin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LJ_potential.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
